0986bad1dae8be407061cc4b1fc94ca941d00c63
Renato Barros <renato.l@lyncas.net>
2022-08-28 13:30:30 -0300

    feat: hook usage: useCallBack, concepts and practice

--- src/hooks/UseCallback/index.jsx
+++ src/hooks/UseCallback/index.jsx
@@ -1,55 +1,40 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import P from 'prop-types';
 
 import '../../App.css';
 
-const eventFn = () => {
-  console.log('h1 clicado');
-};
+// Aqui é usado o hook memo para memorizar o componente e caso necessário não precisar renderizar novamente
+const Button = React.memo(function Button({ incrementButton }) {
+  console.log('Filho, renderizou');
+  return (
+    <button type="button" onClick={() => incrementButton(10)}>
+      Add + 1
+    </button>
+  );
+});
 
 const UseCallBack = () => {
   const [counter, setCounter] = useState(0);
-  const [counter2, setCounter2] = useState(0);
 
-  // // componentDidUpdate - executa toda vez que o componente atualiza
-  // useEffect(() => {
-  //   console.log('componentDidUpdate');
-  // });
-
-  // // componentDidMount - executa uma única vez quando o componente é montado
-  // useEffect(() => {
-  //   console.log('componentDidMount');
-  // }, []);
-
-  // Com dependência - executa toda vez qeu a depêndencia mudar
-  useEffect(() => {
-    console.log('Contador mudou para ', counter);
-  }, [counter]);
-
-  // componentWillUnmount - limpar lixos feitos anteriormente antes que o componente seja desmontado
-  // Uso do useEffect para limpar lixos feitos anteriormente, no caso o removeEventListener
-  useEffect(() => {
-    document.querySelector('h1')?.addEventListener('click', eventFn);
-
-    // componentWillUnmount - limpeza
-    return () => {
-      document.querySelector('h1')?.removeEventListener('click', eventFn);
-    };
+  // Aqui foi usado o valor antigo do useState para evitar uma nova renderização
+  // O useCallback é usado para memorizar a função e melhorar o desempenho evitando uma nova redenrização
+  const incrementCounter = useCallback((num) => {
+    setCounter((prevCounter) => prevCounter + num);
   }, []);
+
+  console.log('Pai, renderizou');
 
   return (
     <div className="App">
       <p> Teste 1 </p>
-      <h1>
-        C1: {counter} C2: {counter2}
-      </h1>
-      <button type="button" onClick={() => setCounter(counter + 1)}>
-        Add + 1
-      </button>
-      <button type="button" onClick={() => setCounter2(counter2 + 1)}>
-        Add + 2
-      </button>
+      <h1>C1: {counter}</h1>
+      <Button incrementButton={incrementCounter} />
     </div>
   );
+};
+
+Button.propTypes = {
+  incrementButton: P.func,
 };
 
 export default UseCallBack;

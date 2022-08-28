2369b7a17b0730b2f910f915efe95ffb2d473d68
Renato Barros <renato.l@lyncas.net>
2022-08-28 16:28:24 -0300

    feat: creating file for hook useMemo

---
+++ src/hooks/UseMemo/index.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import P from 'prop-types';
+
+import '../../App.css';
+
+// Aqui é usado o hook memo para memorizar o componente e caso necessário não precisar renderizar novamente
+const Button = React.memo(function Button({ incrementButton }) {
+  console.log('Filho, renderizou');
+  return (
+    <button type="button" onClick={() => incrementButton(10)}>
+      Add + 1
+    </button>
+  );
+});
+
+const UseMemo = () => {
+  const [counter, setCounter] = useState(0);
+
+  // Aqui foi usado o valor antigo do useState para evitar uma nova renderização
+  // O useCallback é usado para memorizar a função e melhorar o desempenho evitando uma nova redenrização
+  const incrementCounter = useCallback((num) => {
+    setCounter((prevCounter) => prevCounter + num);
+  }, []);
+
+  console.log('Pai, renderizou');
+
+  return (
+    <div className="App">
+      <p> Teste 1 </p>
+      <h1>C1: {counter}</h1>
+      <Button incrementButton={incrementCounter} />
+    </div>
+  );
+};
+
+Button.propTypes = {
+  incrementButton: P.func,
+};
+
+export default UseMemo;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './hooks/UseCallback';
+import App from './hooks/UseMemo';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

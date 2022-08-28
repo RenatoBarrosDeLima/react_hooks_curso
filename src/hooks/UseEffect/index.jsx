c495072fbfdbb13ddca4f49eae472d4eb965ddbb
Renato Barros <renato.l@lyncas.net>
2022-08-28 12:18:03 -0300

    feat: creating file for hook useEffect

---
+++ src/hooks/UseEffect/index.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import '../../App.css';
+
+const UseEffect = () => {
+  return (
+    <div className="App">
+      <h1> OI </h1>
+    </div>
+  );
+};
+
+export default UseEffect;

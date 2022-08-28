ac8163f7018b302767bad35c13f55a42f61a728b
Renato Barros <renato.l@lyncas.net>
2022-08-28 12:16:09 -0300

    feat: creating folders for hooks

---
+++ src/hooks/UseState/index.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import logo from '../../logo.svg';
+import '../../App.css';
+
+const UseState = () => {
+  const [reverse, setReverse] = useState(false);
+  const [counter, setCounter] = useState(0);
+  const classReverse = reverse ? 'reverse' : '';
+
+  const handleClick = () => {
+    // setReverse(!reverse);
+    // Outra forma do useState, usando o callBack do valor atual da função
+    setReverse((prevCounter) => !prevCounter);
+  };
+
+  const handleIncremente = () => {
+    // setCounter(counter + 1);
+    // Outra forma do useState, usando o callBack do valor atual da função
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className={`App-logo ${classReverse}`} alt="logo" />
+
+        <h1> Contador: {counter}</h1>
+
+        <p>
+          <button type="button" onClick={handleClick}>
+            {reverse ? 'Normal' : 'Reverse'}{' '}
+          </button>
+        </p>
+
+        <p>
+          <button type="button" onClick={handleIncremente}>
+            Increment
+          </button>
+        </p>
+      </header>
+    </div>
+  );
+};
+
+export default UseState;

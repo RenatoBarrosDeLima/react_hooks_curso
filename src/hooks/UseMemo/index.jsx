a54a44b8874427571508d50ed7c94dff7ddde558
Renato Barros <renato.l@lyncas.net>
2022-08-28 17:48:32 -0300

    feat: hook usage: useMemo, concepts and practice

--- src/hooks/UseMemo/index.jsx
+++ src/hooks/UseMemo/index.jsx
@@ -1,40 +1,56 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import P from 'prop-types';
 
 import '../../App.css';
 
-// Aqui é usado o hook memo para memorizar o componente e caso necessário não precisar renderizar novamente
-const Button = React.memo(function Button({ incrementButton }) {
-  console.log('Filho, renderizou');
-  return (
-    <button type="button" onClick={() => incrementButton(10)}>
-      Add + 1
-    </button>
-  );
-});
-
-const UseMemo = () => {
-  const [counter, setCounter] = useState(0);
-
-  // Aqui foi usado o valor antigo do useState para evitar uma nova renderização
-  // O useCallback é usado para memorizar a função e melhorar o desempenho evitando uma nova redenrização
-  const incrementCounter = useCallback((num) => {
-    setCounter((prevCounter) => prevCounter + num);
-  }, []);
-
-  console.log('Pai, renderizou');
+const Post = ({ post }) => {
+  console.log('Filho renderizou');
 
   return (
-    <div className="App">
-      <p> Teste 1 </p>
-      <h1>C1: {counter}</h1>
-      <Button incrementButton={incrementCounter} />
+    <div className="post">
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
     </div>
   );
 };
 
-Button.propTypes = {
-  incrementButton: P.func,
+const UseMemo = () => {
+  const [posts, setPosts] = useState([]);
+  const [value, setValue] = useState('');
+
+  // componentDidMount
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((r) => r.json())
+        .then((r) => setPosts(r));
+    }, 2000);
+  }, []);
+
+  console.log('Pai renderizou ');
+
+  return (
+    <div className="App">
+      <p>
+        <input type="search" value={value} onChange={(e) => setValue(e.target.value)} />
+      </p>
+      {/* Aqui o use memo salva a lista enquanto ela não é mudada,
+      o input da pesquisa é alterado mas a lista não é redendizada novamente
+      porque o useMemo memorizou a lista  */}
+      {useMemo(() => {
+        return posts.length > 0 && posts.map((post) => <Post key={post.id} post={post} />);
+      }, [posts])}
+      {posts.length === 0 && <p> Ainda não exitem posts </p>}
+    </div>
+  );
+};
+
+Post.propTypes = {
+  post: P.shape({
+    id: P.number,
+    title: P.string,
+    body: P.string,
+  }),
 };
 
 export default UseMemo;
